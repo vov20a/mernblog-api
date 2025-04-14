c53deabc2d114daa4574d17f6520f866fb31beed
vov20a <vov20a@mail.ru>
2025-04-14 18:02:25 +0300

    allow origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-  'http://localhost:3000',
-  'https://mernshop-w9rp.onrender.com',
-  'https://api.apilayer.com/currency_data/live?source=USD&currencies=EUR%2CRUB',
+  // 'http://localhost:3000',
+  'https://mernblog-api-be61.onrender.com',
+  // 'https://api.apilayer.com/currency_data/live?source=USD&currencies=EUR%2CRUB',
 ];
 
 module.exports = allowedOrigins;
