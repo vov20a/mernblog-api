1bd5bd4ed86b8813e3089439c4bb4d64f84307b7
vov20a <vov20a@mail.ru>
2025-04-14 18:07:46 +0300

    allow origin2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   // 'http://localhost:3000',
-  'https://mernblog-api-be61.onrender.com',
+  'https://mernblog-stvy.onrender.com/',
   // 'https://api.apilayer.com/currency_data/live?source=USD&currencies=EUR%2CRUB',
 ];
 
