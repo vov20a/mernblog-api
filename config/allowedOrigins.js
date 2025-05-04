4411fcd3c8bf86daa46dcfdcd05ff29faaf3d5fe
vov20a <vov20a@mail.ru>
2025-05-04 22:04:53 +0300

    update post-2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  'http://localhost:3000',
-  // 'https://mernblog-stvy.onrender.com',
+  // 'http://localhost:3000',
+  'https://mernblog-stvy.onrender.com',
   // 'https://api.apilayer.com/currency_data/live?source=USD&currencies=EUR%2CRUB',
 ];
 
